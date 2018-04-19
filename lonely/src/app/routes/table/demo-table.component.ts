776cca58e8b44e2f60aa907dbdc45c2aa5f0c3f8
zhaoll <zhaoll@ulopay.com>
2018-04-19 10:16:00 +0800

    ng-alain 简单项目搭建

---
+++ lonely/src/app/routes/table/demo-table.component.ts
@@ -0,0 +1,70 @@
+import {Component, ViewChild} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {SimpleTableColumn, SimpleTableComponent} from '@delon/abc';
+
+@Component({
+    selector:'demo-table',
+    // templateUrl:'./demo-table.component.html'
+    template: `<simple-table [data]="url" [extraParams]="params" [total]="total" [columns]="columns"
+                [resReName]="{list: 'results' }"></simple-table>`
+})
+export class DemoTableComponent{
+    url = `https://randomuser.me/api/?results=3`;
+    // url:string = "/assets/josn/test.org.data.json";
+    params = { a: 1, b: 2 };
+    // mock
+    total = 50;
+    columns: SimpleTableColumn[] = [
+        { title: '编号', index: 'id.value' },
+        { title: '头像', type: 'img', width: '50px', index: 'picture.thumbnail' },
+        { title: '邮箱', index: 'email' },
+        { title: '电话', index: 'phone' },
+        { title: '注册时间', type: 'date', index: 'registered' }
+    ];
+
+
+
+
+    // @ViewChild('table') table: SimpleTableComponent;
+    //
+    // public url:string = "/assets/josn/test.org.data.json";
+    // public htmlStr:string = "<div>123123123</div>";
+    //
+    // constructor(public http:HttpClient){}
+    //
+    // columns:Array<any> = [{
+    //     index:'orgNo',
+    //     title:'机构编号'
+    // },{
+    //     index:'orgName',
+    //     title:'机构名称'
+    // },{
+    //     title:'输入html内容',
+    //     render:'htmlRender'
+    // },{
+    //     title:'按钮',
+    //     buttons:[
+    //         {
+    //             // 编辑
+    //             text: '编辑',
+    //         },
+    //
+    //     ]}
+    // ];
+    //
+    // onSearch(){
+    //     console.log('search....');
+    //     this.url = "/assets/json/test2.org.data.json";
+    //     // this.table.doSearch();
+    // }
+    // expandData:any[]=[];
+    // onExpandChange(val){
+    //     console.log('expandChange:::',val);
+    //     // if(!this.expandData[val.index]){
+    //     this.http.get('/assets/json/test2.org.data.json').subscribe(res=>{
+    //         this.expandData[val.index] = res;
+    //         this.expandData[val.index]['status'] = Math.floor(Math.random() * 10);
+    //     });
+    //     // }
+    // }
+}
